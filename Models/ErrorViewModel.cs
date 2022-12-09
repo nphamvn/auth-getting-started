a474401db7dac94e190776769f28e8808a95a233
nampham <pvnam95@hotmail.com>
2022-12-09 22:43:10 +0900

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AuthGettingStarted.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
